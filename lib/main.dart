476a9bbced3e097077900b3edaf0d3d4446a9dea
Elian Ortega <elian.mortega@gmail.com>
2021-04-01 23:26:40 -0600

    First commit project setup

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bluetooth_scanner/src/app.dart';
+
+void main() {
+  runApp(BluetoothScannerApp());
+}
---
+++ lib/src/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BluetoothScannerApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Scaffold(),
+    );
+  }
+}
